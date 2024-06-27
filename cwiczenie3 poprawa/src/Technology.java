3d25ff00e613c63785af3d75d30b54bc58137287
unknown <s23276@pjwstk.edu.pl>
2024-06-27 18:34:52 +0200

    Cwiczenie3 poprawa

--- cwiczenie3 poprawa/src/Technology.java
+++
@@ -1,9 +0,0 @@
-public class Technology {
-    public String name;
-    public int bonus;
-
-    public Technology(String name, int bonus) {
-        this.name = name;
-        this.bonus = bonus;
-    }
-}
